1de20b82677e7224356ca64a89b55fef12f13427
urtblw <dgoskiy@gmail.com>
2024-04-15 00:39:46 +0700

    1.1.5

--- index.js
+++ index.js
@@ -19,25 +19,99 @@ const app = express()
 
 app.use(express.json())
 
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email, })
+
+        if (!user) {
+            return req.status(404).json({
+                message: "Пользователь не найден"
+            })
+        }
+
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+
+        if (!isValidPass) {
+            return req.status(404).json({
+                message: "Неверный логин или пароль"
+            })
+        }
+
+        const token = jwt.sign(
+            {
+                _id: user._id
+            },
+            'secret123',
+            {
+                expiresIn: "30d"
+            }
+        )
+
+        const { passwordHash, ...userData } = user._doc
+
+        res.json({
+            ...userData,
+            token
+        })
+
+
+
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: "Не удалось авторизоваться"
+        })
+    }
+
+
+
+})
+
+
+
 app.post('/auth/register', registerValidation, async (req, res) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) res.status(400).json(errors.array())
+    try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) res.status(400).json(errors.array())
 
-    const password = req.body.password
-    const salt = await bcrypt.genSalt(10)
-    const passwordHash = await bcrypt.hash(password, salt)
+        const password = req.body.password
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
 
-    const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.email,
-        avatarUrl: req.body.avatarUrl,
-        passwordHash,
-    })
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash: hash,
+        })
 
-    const user = await doc.save()
+        const user = await doc.save()
 
-    res.json(user)
+        const token = jwt.sign(
+            {
+                _id: user._doc._id
+            },
+            "secret1234",
+            {
+                expiresIn: "30d"
+            }
+        );
 
+
+        const { passwordHash, ...userData } = user._doc
+        res.json({
+            ...userData,
+            token
+        })
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: "Не удалось зарегетрироваться"
+        })
+    }
 
 })
 
