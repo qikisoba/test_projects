579f5f609f1ae96e8ab6dfe4be0e4b867aa23010
qikisoba <165438359+qikisoba@users.noreply.github.com>
2024-04-15 00:56:25 +0700

    1.1.6

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.post('/auth/login', async (req, res) => {
         const user = await UserModel.findOne({ email: req.body.email, })
 
         if (!user) {
-            return req.status(404).json({
+            return res.status(404).json({
                 message: "Пользователь не найден"
             })
         }
