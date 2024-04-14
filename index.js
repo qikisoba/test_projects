0630659deea13ece2c647aed59fa659bdd1d0994
urtblw <dgoskiy@gmail.com>
2024-04-14 21:16:25 +0700

    1.1.2

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from "express"
 import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
 import mongoose from "mongoose"
+
 import { registerValidation } from "./validations/auth.js"
+
 import { validationResult } from 'express-validator'
 
+import { UserModel } from './models/User.js'
 
 mongoose
     .connect('mongodb+srv://iykisoba:artur42r@cluster0.reryft7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
@@ -14,13 +19,25 @@ const app = express()
 
 app.use(express.json())
 
-app.post('/auth/register', registerValidation, (req, res) => {
+app.post('/auth/register', registerValidation, async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) res.status(400).json(errors.array())
 
-    res.json({
-        success: true
+    const password = req.body.password
+    const salt = await bcrypt.genSalt
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.email,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash: req.body.passwordHash,
+
     })
+
+    const user = await doc.save()
+
+    res.json(user)
 
 
 
