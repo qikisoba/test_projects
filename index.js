c9c845b87b001c269e9bd5ead24390ed1b7f831d
urtblw <dgoskiy@gmail.com>
2024-04-15 19:02:22 +0700

    1.1.9

--- index.js
+++ index.js
@@ -1,164 +1,31 @@
 import express from "express"
-import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
-
 import mongoose from "mongoose"
-
-import { registerValidation } from "./validations/auth.js"
-
-import { validationResult } from 'express-validator'
-
-import UserModel from './models/User.js'
+import { postCreateValidation, loginValidation, registerValidation } from "./validations.js"
 import checkAuth from './utils/checkAuth.js'
-
-
+import * as UseController from './controllers/UseController.js'
+import * as PostController from './controllers/PostController.js'
 mongoose
   .connect('mongodb+srv://iykisoba:artur42r@cluster0.reryft7.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => console.log('DB ok'))
   .catch((err) => console.log("DB error", err))
 
 const app = express()
-
 app.use(express.json())
+app.post('/auth/login', loginValidation, UseController.login)
+app.post('/auth/register', registerValidation, UseController.register)
+app.get('/auth/me', checkAuth, UseController.getMe)
 
-app.post('/auth/login', async (req, res) => {
-  try {
-    const user = await UserModel.findOne({ email: req.body.email, })
-
-    if (!user) {
-      return res.status(404).json({
-        message: "Пользователь не найден"
-      })
-    }
-
-
-    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
-
-    if (!isValidPass) {
-      return res.status(403).json({
-        message: "Неверный логин или пароль"
-      })
-    }
+app.get('/posts/all', PostController.getAll)
+// app.get('/post/create/:id', PostController.getOne)
+app.post('/posts/create', checkAuth, postCreateValidation, PostController.create)
+// app.delete('/posts', PostController.remove)
+// app.patch('/posts', PostController.update)
 
 
-
-    const token = jwt.sign(
-      {
-        _id: user._id
-      },
-      'secret123',
-      {
-        expiresIn: "30d"
-      }
-    )
-
-    const { passwordHash, ...userData } = user._doc
-
-    res.json({
-      ...userData,
-      token
-    })
-
-
-
-
-  } catch (err) {
-    console.log(err)
-    res.status(500).json({
-      message: "Не удалось авторизоваться"
-    })
-  }
-
-
-
-})
-
-
-
-app.post('/auth/ebasit', (req, res) => {
-  console.log(req.body)
-  const token = jwt.sign({
-    diman: "daway",
-    use: "ebasit"
-  }, "secret123")
-  res.json({
-    succes: true,
-    token
-  })
-})
-
-app.post('/auth/register', registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) res.status(400).json(errors.array())
-
-    const password = req.body.password
-    const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash(password, salt)
-
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash: hash,
-    })
-
-    const user = await doc.save()
-
-    const token = jwt.sign(
-      {
-        _id: user._doc._id
-      },
-      "secret1234",
-      {
-        expiresIn: "30d"
-      }
-    );
-
-
-    const { passwordHash, ...userData } = user._doc
-    res.json({
-      ...userData,
-      token
-    })
-
-  } catch (err) {
-    console.log(err)
-    res.status(500).json({
-      message: "Не удалось зарегистрироваться"
-    })
-  }
-
-})
-
-
-app.get('/auth/me', checkAuth, async (req, res) => {
-  try {
-    const user = await UserModel.findById(req.userId)
-    if (!user) {
-      return res.status(404).json({
-        message: "Пользователь не найден"
-      })
-    }
-
-
-    const { passwordHash, ...userData } = user._doc
-    res.json(userData)
-
-  } catch (err) {
-
-    res.status(500).json({
-      message: "Нет доступа"
-    })
-  }
-})
-
+// app.post('/auth/ebasit', ebasit)
 app.listen(4444, (err) => {
-
   if (err) {
     return console.log("Ошибка!!!")
   }
-
   console.log("Server ОК")
-
 })
--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const UserShema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     fullName: {
         type: String,
         required: true
@@ -19,4 +19,4 @@ const UserShema = new mongoose.Schema({
     timestamps: true,
 });
 
-export default mongoose.model("User", UserShema)
+export default mongoose.model("User", UserSchema)
