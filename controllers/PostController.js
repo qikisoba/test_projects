16ab182fd3b080fdf15d1dc9f0c242ffe0d0cb75
urtblw <dgoskiy@gmail.com>
2024-04-18 13:28:34 +0300

    add update

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -102,3 +102,32 @@ export const remove = async (req, res) => {
     });
   }
 };
+
+
+
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    await PostModel.updateOne(
+      {
+        _id: postId
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        tags: req.body.tags,
+        user: req.userId
+      }
+    )
+    res.json({
+      success: true
+    })
+
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({
+      message: "Не удалось обновить статью"
+    })
+  }
+}
--- index.js
+++ index.js
@@ -1,26 +1,46 @@
 import express from "express"
 import mongoose from "mongoose"
+import multer from "multer"
 import { postCreateValidation, loginValidation, registerValidation } from "./validations.js"
 import checkAuth from './utils/checkAuth.js'
 import * as UseController from './controllers/UseController.js'
 import * as PostController from './controllers/PostController.js'
 mongoose
   .connect('mongodb+srv://iykisoba:artur42r@cluster0.reryft7.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
+  // .connect('mongodb+srv://dgoskiy:artur42r@cluster0.tohy41e.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
+
   .then(() => console.log('DB ok'))
   .catch((err) => console.log("DB error", err))
 
-// 111
 const app = express()
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
 app.use(express.json())
 app.post('/auth/login', loginValidation, UseController.login)
 app.post('/auth/register', registerValidation, UseController.register)
 app.get('/auth/me', checkAuth, UseController.getMe)
 
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+})
+
+app.post('/posts', checkAuth, postCreateValidation, PostController.create)
 app.get('/posts', PostController.getAll)
 app.get('/posts/:id', PostController.getOne)
-app.post('/posts', checkAuth, postCreateValidation, PostController.create)
 app.delete('/posts/:id', checkAuth, PostController.remove)
-// app.patch('/posts', PostController.update)
+app.patch('/posts/:id', checkAuth, PostController.update)
 
 
 // app.post('/auth/ebasit', ebasit)
